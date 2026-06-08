3ec905312243050a37600394d16b792a74f393f5
Jacob Wolford <142846333+Real-Mr-Wolford@users.noreply.github.com>
2026-06-08 15:17:41 -0500

    Improve screen recorder initialization and messages

--- screenrec.py
+++ screenrec.py
@@ -7,6 +7,7 @@ import time
 def start_screen_recorder(output_filename="screen_record.mp4", fps=24.0):
     screen_size = ImageGrab.grab().size
     
+    
     fourcc = cv2.VideoWriter_fourcc(*"mp4v") #here you can choose any codec you want. If you want to switch to .avi, replace *"mp4v" with *"avi". Here are some more codecs
     #you can use. Be sure to change the filename extension as well :) 
     # MP4 File Container (.mp4 extension)
@@ -34,36 +35,41 @@ def start_screen_recorder(output_filename="screen_record.mp4", fps=24.0):
     
     out = cv2.VideoWriter(output_filename, fourcc, fps, screen_size)
     
-    print("\n[!] Screen Recording")
+    print("\n[!] Screen Recorder Initialized.")
     print(f"[!] Resolution: {screen_size[0]}x{screen_size[1]} | Target FPS: {fps}")
     print("[!] PRESS 'q' INSIDE THE VIDEO WINDOW TO STOP RECORDING...\n")
     
-    for i in range(3, 0, -1):
-        print(f"Starting in {i}...")
-        time.sleep(1)
+    
 
     try:
         while True:
             img = ImageGrab.grab()
+            
+           
             img_np = np.array(img)
-        
+            
+           
             frame = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+            
+           
             out.write(frame)
             
+          
             small_frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
             cv2.imshow("TerminalOS - Recording Preview", small_frame)
             
+           
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
                 
     except KeyboardInterrupt:
-        print("\n[!] Recording interrupted by user.")
+        print("\nRecording stopped.")
         
     finally:
         
         out.release()
         cv2.destroyAllWindows()
-        print(f"\n[+] Success! Recording saved as: {os.path.abspath(output_filename)}")
+        print(f"\nRecording saved as: {os.path.abspath(output_filename)}")
 
 if __name__ == "__main__":
     start_screen_recorder()
